c44a2397557c249e4179411d34c0e04a675bbef9
Kotha Hemanth <hemanthkotha17@gmail.com>
2024-03-07 11:54:50 +0530

    Step-4

--- webapp/index.js
+++ webapp/index.js
@@ -1,22 +1,26 @@
 sap.ui.require([
-	"sap/m/Text",
-	"sap/ui/model/json/JSONModel"
-], function (Text, JSONModel) {
+	"sap/ui/model/json/JSONModel",
+	"sap/ui/core/mvc/XMLView"
+], function (JSONModel, XMLView) {
 	"use strict";
 
 	// Attach an anonymous function to the SAPUI5 'init' event
 	sap.ui.getCore().attachInit(function () {
 		// Create a JSON model from an object literal
 		var oModel = new JSONModel({
-			greetingText: "Hi, My Name is Kotha Venkata Hemanth"
-		});
+			firstName: "Kotha",
+			lastName: "Venkata Hemanth",
+			enabled: true,
+			panelHeaderText: "Data Binding Basics"
 
+		});
 		// Assign the model object to the SAPUI5 core
 		sap.ui.getCore().setModel(oModel);
 
+		// Display the XML view called "App"
+		new XMLView({
+			viewName: "sap.ui.demo.db.view.App"
+		}).placeAt("content");
 
-		// Display a text element whose text is derived
-		// from the model object
-		new Text({text:"{/greetingText}"}).placeAt("content");
 	});
 });
